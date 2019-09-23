fc744269694e8f29573ef02b3145814b1cf24ce3
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-09-23 17:57:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 def line(deliline)
   if deliline.empty?
     puts "The line is currently empty."
-  else current_line = "The line is currently:"
+  else current_line= "The line is currently:"
     deliline.each.with.index(1) do |person,i|
       current_line << "#{i}. #{person}"
     end
