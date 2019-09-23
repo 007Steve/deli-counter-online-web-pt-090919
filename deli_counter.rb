684440056e294179c227d9131f8a1bfe67defbf9
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-09-23 17:53:29 +0000

    Automatically backed up by Learn

---
+++ deli_counter.rb
@@ -0,0 +1,10 @@
+def line(deliline)
+  if deliline.empty?
+    puts "The line is currently empty."
+  else current_line = "The line is currently:"
+    deliline.each.with.index(1) do |person,i|
+      current_line << "#{i}. #{person}"
+    end
+    puts current_line
+  end
+end
